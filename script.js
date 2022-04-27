9c0528b57c46929031d5aefd1c8a9d8cab81b082
andrewssito <aksito1102@gmail.com>
2022-04-27 00:22:20 -0700

    randomized pokemon retrieval js set

--- script.js
+++ script.js
@@ -4,10 +4,19 @@
 function getPokemon() {
 
 
+    function getRandomNumber(max) {
+        return Math.floor(Math.random()*max);
+        
+    }
+    const number = getRandomNumber(500);
+
+
+    console.log(number);
+
     {
-        axios.get("https://pokeapi.co/api/v2/pokemon")
+        axios.get(`https://pokeapi.co/api/v2/pokemon/${number}`)
         .then(result => {
-            console.log(result.data);
+            console.log(result.data.name);
         })
         .catch(error => {
             console.log(error);
@@ -15,6 +24,15 @@ function getPokemon() {
 
     }
 
+
+    /* 
+    click button
+    grab pokemon name
+    print pokemon name (maybe in like a sentence) on the page
+    (probably need a clear function somewhere)
+    everytime u refresh / press the button again, a new pokemon will replace it
+
+    */
 
 }
 
